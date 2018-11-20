fe7b46467f0a1847f88a936627eeb2b366bf4330
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-20 14:03:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,10 @@ function increaseRankBy(n){
   const rankedLists = document.querySelectorAll('.ranked-list')
 
   for (let i = 0; i < rankedLists.length; i++){
-    var lis = rankedLists[i];
-  }
-  for (let a = 0; a < lis.length; a++){
-    lis[a].innerHTML = parseInt(lis[a]) + n;
+    var lis = rankedLists[i].children;
+
+    for (let a = 0; a < lis.length; a++){
+      lis[a].innerHTML = parseInt(lis[a]) + n;
+    }
   }
 }
